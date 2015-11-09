92cc05d0a4e2594f505dc01077921df47084ea64
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-09 03:48:24 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,31 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+  
+  def initialize(number)
+    @number = number
+  end
+  
+  def difference
+
+  mynum = @number
+  sum_of_squares = 0
+  sum = 0
+
+  until mynum == 0
+    sum_of_squares = sum_of_squares + mynum ** 2
+    sum += mynum
+puts "#{mynum} #{sum_of_squares} #{sum}"
+    mynum -= 1
+  end
+puts "#{mynum} #{sum_of_squares} #{sum}"
+answer = sum ** 2 - sum_of_squares
+end
+
+  
+  
+  
+  
+  
+  
+  
+end
+
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+def sum_square_difference(number)
+
+  mynum = number
+  sum_of_squares = 0
+  sum = 0
+
+  until mynum == 0
+    sum_of_squares = sum_of_squares + mynum ** 2
+    sum += mynum
+puts "#{mynum} #{sum_of_squares} #{sum}"
+    mynum -= 1
+  end
+puts "#{mynum} #{sum_of_squares} #{sum}"
+answer = sum ** 2 - sum_of_squares
+end
